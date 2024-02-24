3df06d04cb829b63f8b206783835956ff5d5084c
Özkan Büyük <ozkanbuyuk@icloud.com>
2024-02-24 06:14:33 +0300

    React - Interactive Comments

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles/reset.scss";
+import "./styles/variables.scss";
+import "./styles/global.scss";
+
+import Data from "../data.json";
+
+import { Conversation } from "./components/conversation";
+
+function App() {
+  return (
+    <div className="container">
+      <Conversation {...Data} />
+    </div>
+  );
+}
+
+export default App;
